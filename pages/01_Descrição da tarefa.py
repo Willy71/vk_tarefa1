27a5f9fc29d35710b3def971fbd12a34f58d805b
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-10-21 11:53:51 -0300

    Update 01_Descrição da tarefa.py

--- pages/01_Descrição da tarefa.py
+++ pages/01_Descrição da tarefa.py
@@ -1,47 +1,4 @@
 import streamlit as st
 import st_format as sf
 
-sf.text_right("
-
-Nossa ideia é te inserir num ambiente prático que você viverá e te dar liberdade para fazer o melhor que você que conseguir, independente do que for.
-
-Analisaremos:
-- Se sua análise de fato se transformou em insights úteis para o lançamento
-- Quanto as conclusões geradas dos dados estavam corretas
-- Quanto capricho visual e organizacional você trouxe
-- Quão bem você argumentou sobre os pontos que trouxe
-- Quão profundo e criativo você foi em sua análise
-
-Segue a descrição e link para as tabelas.
-
-A primeira tabela de dados traz dados de leads inscritos em um lançamento, ou seja, dados de quem se inscreveu para participar de um lançamento.
-
-Na tabela há o e-mail do lead e os parâmetros de UTM de entrada desse lead. Dica: UTMs são parâmetros que indicam a origem do lead, nesse caso, Source representa o canal de origem, Medium (para canais de tráfego pago) representa o público e Term (para canais de tráfego pago) o anúncio que trouxe o lead.
-
-001_001_TabelaPesquisaUTMsn.csv
-
-https://drive.google.com/file/d/1volT93MoYgn6MvC5UzWwEN3fLQIoFLsB/view?usp=sharing
-
-A segunda tabela traz dados de compradores do lançamento.
-
-Ela é composta apenas pelo e-mail de quem comprou a oferta realizada no lançamento.
-
-002_TabelaVendas.csv
-
-https://drive.google.com/file/d/1ycy2ChfmycwoyqXvbdzpq3SiiEmeZD6q/view?usp=sharing
-
-A terceira tabela traz dados de uma pesquisa de persona distribuída durante a captação e que foi respondida por alguns dos leads dando mais informações sobre eles.
-
-Ela é composta apenas pelo e-mail do lead que respondeu, idade, renda e há quanto tempo conhece o expert.
-
-003_TabelaPesquisa.csv
-
-https://drive.google.com/file/d/1C8St7Fp9SWXRogMlZ7L8bc8mHabEWzNi/view?usp=sharing
-
-A quarta tabela contém o link pra alguns dos anúncios retratados como origem dos leads.
-
-O anúncio em si pode servir para análises mais qualitativas do seu estudo.
-
-https://drive.google.com/file/d/1tlXqjjj9B0mzNnIu7wXmlC4HNAm26Gvc/view?usp=sharing
-
-Imagine que você está numa equipe de BI desse projeto. Com base nos dados coletados, monte um estudo que dê uma visão clara dos resultados do lançamento para os stakeholders e que também já levante possíveis otimizações para melhora de resultado em próximos lançamentos.", 2, "white")
+sf.text_right("Nossa ideia é te inserir num ambiente prático que você viverá e te dar liberdade", 2, "white")
