70f0783acd0be5476d417b56b9263d2bc8b8a93c
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-10-21 11:54:12 -0300

    Update 01_Descrição da tarefa.py

--- pages/01_Descrição da tarefa.py
+++ pages/01_Descrição da tarefa.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import st_format as sf
 
-sf.text_right("Nossa ideia é te inserir num ambiente prático que você viverá e te dar liberdade", 2, "white")
+sf.text_left("Nossa ideia é te inserir num ambiente prático que você viverá e te dar liberdade", 2, "white")
