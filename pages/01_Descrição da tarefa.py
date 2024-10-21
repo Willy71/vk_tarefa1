654a2fa4bb3466c905611a60b7930bdbac15db30
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-10-21 18:03:41 -0300

    Update 01_Descrição da tarefa.py

--- pages/01_Descrição da tarefa.py
+++ pages/01_Descrição da tarefa.py
@@ -1,6 +1,24 @@
 import streamlit as st
 import st_format as sf
 
+st.set_page_config(
+    page_title="Silvertech - Trabajo final",
+    page_icon="🌌",
+    layout="wide"
+)
+
+# We reduced the empty space at the beginning of the streamlit
+reduce_space ="""
+            <style type="text/css">
+            /* Remueve el espacio en el encabezado por defecto de las apps de Streamlit */
+            div[data-testid="stAppViewBlockContainer"]{
+                padding-top:30px;
+            }
+            </style>
+            """
+# We load reduce_space
+st.html(reduce_space)
+
 sf.line(5, "blue")
 sf.center_text("Descrição da tarefa", 3, "white")
 sf.line(5, "blue")
