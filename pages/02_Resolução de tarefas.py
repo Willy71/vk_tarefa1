435e7bc1c1126030b8a07c8ce98a19841712e859
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-10-21 15:06:36 -0300

    Update 02_Resolução de tarefas.py

--- pages/02_Resolução de tarefas.py
+++ pages/02_Resolução de tarefas.py
@@ -78,11 +78,11 @@ with st.container():
     sf.text_left("Colunas principais:", 4, "lightgrey")
 with st.container():
 col07, col08 = st.columns([0.5,6]) 
-with col08:
-  st.markdown("email: E-mail do lead.")
-  st.markdown("utmsource: Canal de origem do lead (ex: 'facebookads').")
-  st.markdown("utmmedium: Público do tráfego pago (ex: 'lookalike_1_compras_180D').")
-  st.markdown("utmterm: Anúncio específico que trouxe o lead (ex: 'AD17').")
+  with col08:
+    st.markdown("email: E-mail do lead.")
+    st.markdown("utmsource: Canal de origem do lead (ex: 'facebookads').")
+    st.markdown("utmmedium: Público do tráfego pago (ex: 'lookalike_1_compras_180D').")
+    st.markdown("utmterm: Anúncio específico que trouxe o lead (ex: 'AD17').")
 
 
 
