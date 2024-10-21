28c679d35a83c81b6ca9ff798823b64877876d9a
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-10-21 11:28:20 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import st_format as sf
 
 sf.line(5, "blue")
-sf.center_text("Tarefas para VK", 2, "lightgrey")
+sf.center_text("Tarefa 1 para VK", 2, "lightgrey")
 sf.line(5, "blue")
 
 
