7f059f079348331845e367de3fb4c92e5b4a5c81
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-10-21 18:04:27 -0300

    Update 02_Resolução de tarefas.py

--- pages/02_Resolução de tarefas.py
+++ pages/02_Resolução de tarefas.py
@@ -2,6 +2,24 @@ import streamlit as st
 import st_format as sf
 import pandas as pd
 
+st.set_page_config(
+    page_title="Silvertech - Trabajo final",
+    page_icon="🌌",
+    layout="wide"
+)
+
+# We reduced the empty space at the beginning of the streamlit
+reduce_space ="""
+            <style type="text/css">
+            /* Remueve el espacio en el encabezado por defecto de las apps de Streamlit */
+            div[data-testid="stAppViewBlockContainer"]{
+                padding-top:30px;
+            }
+            </style>
+            """
+# We load reduce_space
+st.html(reduce_space)
+
 
 sf.text_left("Para montar um estudo com base nos dados fornecidos, vou estruturar o análise em três grandes etapas:", 4, "white")
 
