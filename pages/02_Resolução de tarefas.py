c1bf1d763724746edcce49415bea764782fa34ff
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-10-21 16:48:31 -0300

    Update 02_Resolução de tarefas.py

--- pages/02_Resolução de tarefas.py
+++ pages/02_Resolução de tarefas.py
@@ -159,9 +159,9 @@ conversion_rate = leads_df['comprador'].mean()
 total_leads = len(leads_df)
 total_compradores = leads_df['comprador'].sum()
 
-sf.text_left(f"Total de leads:{total_leads}", 5, "white")
-sf.text_left(f"Total de compradores:{total_compradores}", 5, "white")
-sf.text_left(f"Conversão: {(conversion_rate*100):.2f}%", 5, "white")
+sf.text_left(f"Total de leads:   {total_leads}", 5, "white")
+sf.text_left(f"Total de compradores:   {total_compradores}", 5, "white")
+sf.text_left(f"Conversão:  {(conversion_rate*100):.2f}%", 5, "white")
 
 st.text("")
 
