8bf8d6a86b97d305fb67c924c6339881dffcb3ed
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-10-21 16:57:54 -0300

    Update 02_Resolução de tarefas.py

--- pages/02_Resolução de tarefas.py
+++ pages/02_Resolução de tarefas.py
@@ -158,9 +158,9 @@ conversion_rate = leads_df['comprador'].mean()
 # Contar o número total de leads e compradores
 total_leads = len(leads_df)
 total_compradores = leads_df['comprador'].sum()
-espacio = "     "
 
-sf.text_left(f"Total de leads:{espacio}{total_leads}", 5, "white")
+sf.text_left(f"Total de leads:{total_leads}", 5, "white")
+st.markdown(f"<span style='margin-right: 20px;'>Total de leads:</span> {total_leads}")
 sf.text_left(f"Total de compradores:{total_compradores}", 5, "white")
 sf.text_left(f"Conversão:\t\t\t{(conversion_rate*100):.2f}%", 5, "white")
 
